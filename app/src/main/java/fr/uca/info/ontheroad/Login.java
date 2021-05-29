a2442890940d688ed52461c0cc5e6fd43d77f407
Christophe <pro.chrisrln@gmail.com>
2021-05-30 01:40:21 +0200

    Chris : [TASK-006] Login.

---
+++ app/src/main/java/fr/uca/info/ontheroad/Login.java
@@ -0,0 +1,16 @@
+package fr.uca.info.ontheroad;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
