c88d5faec8e414efaf4b3476d4290cbb29705a01
Christophe <pro.chrisrln@gmail.com>
2021-05-30 01:55:56 +0200

    Chris : [TASK-006] Temporary redirect.

--- app/src/main/java/fr/uca/info/ontheroad/Login.java
+++ app/src/main/java/fr/uca/info/ontheroad/Login.java
@@ -8,9 +8,21 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    private Button login;
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
