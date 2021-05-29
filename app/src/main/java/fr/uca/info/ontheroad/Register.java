d34e4e901c71c5423086847cc996ea823ac9425d
Christophe <pro.chrisrln@gmail.com>
2021-05-30 01:55:08 +0200

    Chris : [TASK-007] Temporary redirect.

--- app/src/main/java/fr/uca/info/ontheroad/Register.java
+++ app/src/main/java/fr/uca/info/ontheroad/Register.java
@@ -1,13 +1,29 @@
 package fr.uca.info.ontheroad;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
+
+    private Button register;
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        register = (Button) findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
