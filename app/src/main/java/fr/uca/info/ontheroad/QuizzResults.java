c4eb6d4d3a65e2b7b7930510bbabedfee4ef3a92
stevestrz <pro.stevestrz@gmail.com>
2021-05-29 14:52:06 +0200

    QuizzHome functions

---
+++ app/src/main/java/fr/uca/info/ontheroad/QuizzResults.java
@@ -0,0 +1,14 @@
+package fr.uca.info.ontheroad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuizzResults extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quizz_results);
+    }
+}
