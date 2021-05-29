924465fa5ffdb455fc9889b4e08dfa7b34af13a9
stevestrz <pro.stevestrz@gmail.com>
2021-05-29 21:54:32 +0200

    bite

---
+++ app/src/main/java/fr/uca/info/ontheroad/UserProfil.java
@@ -0,0 +1,12 @@
+package fr.uca.info.ontheroad;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserProfil extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
