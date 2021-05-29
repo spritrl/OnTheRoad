63f0ae4200ba9e978490260ee59c6fda1a804a2e
Christophe <pro.chrisrln@gmail.com>
2021-05-30 01:42:09 +0200

    Chris : [TASK-007] Register.

---
+++ app/src/main/java/fr/uca/info/ontheroad/Register.java
@@ -0,0 +1,13 @@
+package fr.uca.info.ontheroad;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
