512e8d177e57c1cfb25c129ef6cd3f9ae7aeb482
Christophe <pro.chrisrln@gmail.com>
2021-05-30 21:43:15 +0200

    Chris: [TASK-011] Register with API.

--- app/src/main/java/fr/uca/info/ontheroad/Register.java
+++ app/src/main/java/fr/uca/info/ontheroad/Register.java
@@ -2,8 +2,10 @@ package fr.uca.info.ontheroad;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,13 +18,37 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
 
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText mail = (EditText) findViewById(R.id.mail);
+        EditText password = (EditText) findViewById(R.id.password);
+
+        username.getText();
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
         register = (Button) findViewById(R.id.register);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Register.this, MainActivity.class);
-                startActivity(intent);
+                APIMethode api;
+                String url = "http://192.168.1.42:8080/api/register/username=\""
+                        + username.getText() + "\"&mail=\""
+                        + mail.getText() + "\"&password=\""
+                        + password.getText() + "\"";
+                api = new APIMethode(url);
+                try {
+                    if(api.sendGet()) {
+                        Intent intent = new Intent(Register.this, Welcome.class);
+                        startActivity(intent);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
