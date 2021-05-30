5a6abe03f69040ae88e2ec0b12a9143df08e0019
Christophe <pro.chrisrln@gmail.com>
2021-05-30 21:42:10 +0200

    Chris: [TASK-010] Login with the API.

---
+++ app/src/main/java/fr/uca/info/ontheroad/APIMethode.java
@@ -0,0 +1,92 @@
+package fr.uca.info.ontheroad;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class APIMethode {
+    String url;
+
+    public APIMethode(String url){
+        this.url = url;
+    }
+
+    public Boolean sendGet() throws Exception {
+        Boolean responseToReturn = false;
+        HttpURLConnection httpClient =
+                (HttpURLConnection) new URL(this.url).openConnection();
+
+        // optional default is GET
+        httpClient.setRequestMethod("GET");
+
+        //add request header
+        httpClient.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = httpClient.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + this.url);
+        System.out.println("Response Code : " + responseCode);
+
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(httpClient.getInputStream()))) {
+
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+
+            //print result
+            responseToReturn = Boolean.valueOf(response.toString());
+
+        }
+        return responseToReturn;
+    }
+
+    public void sendPost() throws Exception {
+
+        // url is missing?
+        //String url = "https://selfsolve.apple.com/wcResults.do";
+        String url = "https://httpbin.org/post";
+
+        HttpsURLConnection httpClient = (HttpsURLConnection) new URL(this.url).openConnection();
+
+        //add reuqest header
+        httpClient.setRequestMethod("POST");
+        httpClient.setRequestProperty("User-Agent", "Mozilla/5.0");
+        httpClient.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+        // Send post request
+        httpClient.setDoOutput(true);
+        try (DataOutputStream wr = new DataOutputStream(httpClient.getOutputStream())) {
+            wr.writeBytes(urlParameters);
+            wr.flush();
+        }
+
+        int responseCode = httpClient.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + this.url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(httpClient.getInputStream()))) {
+
+            String line;
+            StringBuilder response = new StringBuilder();
+
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+
+            //print result
+            System.out.println(response.toString());
+
+        }
+
+    }
+}
--- app/src/main/java/fr/uca/info/ontheroad/Login.java
+++ app/src/main/java/fr/uca/info/ontheroad/Login.java
@@ -2,10 +2,13 @@ package fr.uca.info.ontheroad;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class Login extends AppCompatActivity {
     private Button login;
@@ -14,14 +17,33 @@ public class Login extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+        username.getText();
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
         login = (Button) findViewById(R.id.login);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
+                APIMethode api;
+                String url = "http://192.168.1.42:8080/api/login/username=\"" + username.getText() + "\"&password=\"" + password.getText() + "\"";
+                api = new APIMethode(url);
+                try {
+                    if(api.sendGet()) {
+                        Intent intent = new Intent(Login.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
