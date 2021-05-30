a5d941a1a736b56b929ea39bc2949084a5c8ece2
Christophe <pro.chrisrln@gmail.com>
2021-05-30 21:53:31 +0200

    Chris: [TASK-013] Create the REST API.

---
+++ OnTheRoadApi/src/main/java/Application/Application.java
@@ -0,0 +1,18 @@
+package Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan({"Controller"})
+@EntityScan("Entity")
+@EnableJpaRepositories("Repository")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
